e62bede049357f8af067dbd6fbd5a1f01441f4ac
carlosteixeiraa <carlosteixeira.email@gmail.com>
2017-11-24 14:31:48 +0000

    ..

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ function contar (valor) {
 	return valor > 9 ? valor : "0" + valor; 
 }
 
-
 setInterval( function() {
 	document.getElementById("tempoS").innerHTML = contar(++segundos%60);
     document.getElementById("tempoM").innerHTML = contar(parseInt(segundos/60,10));
