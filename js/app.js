a04fdce96bb9f1694d82dacf017a3b38f5b5120e
carlosteixeiraa <carlosteixeira.email@gmail.com>
2017-11-24 14:29:44 +0000

    ..

---
+++ js/app.js
@@ -0,0 +1,21 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('Particulas carregadas.');
+});
+
+var musica = new Audio('nyan.mp3');
+
+window.onload = function tocar() {
+	musica.play();
+}
+
+var segundos = 0;
+
+function contar (valor) { 
+	return valor > 9 ? valor : "0" + valor; 
+}
+
+
+setInterval( function() {
+	document.getElementById("tempoS").innerHTML = contar(++segundos%60);
+    document.getElementById("tempoM").innerHTML = contar(parseInt(segundos/60,10));
+}, 1000);
